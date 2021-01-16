df9560913c52ff1bc3d5dbd014da23f8617a6c88
AbdElRahman ElGamil <abdurrumanmohamed@gmail.com>
2021-01-16 22:02:08 +0200

    lbp optimized

--- source/features/LBP.py
+++ source/features/LBP.py
@@ -15,8 +15,9 @@ class LocalBinaryPatterns:
         # of the image, and then use the LBP representation
         # start = time.time()
         # to build the histogram of patterns
-        lbp = feature.local_binary_pattern(image, self.numPoints,
-            self.radius, method="default")
+        lbp = get_lbp(image)
+        #lbp = feature.local_binary_pattern(image, self.numPoints,
+         #   self.radius, method="default")
         # lbp = lbp_custom(image)
         # print("lbp time", time.time() - start)
         # start = time.time()
@@ -115,3 +116,42 @@ def lbp_calculated_pixel(img, x, y, r = 3):
         val += val_ar[i] * power_val[i] 
           
     return val 
+
+
+
+def get_lbp(img):
+    # Get image dimensions
+    height, width = img.shape
+
+    # Initialize LBP image
+    lbp = np.zeros((height, width), dtype=np.uint8)
+
+    # Directions
+    v = 3
+    dx = [0, v, v, v, 0, -v, -v, -v]
+    dy = [v, v, 0, -v, -v, -v, 0, v]
+
+    # Loop over the 8 neighbors
+    for i in range(8):
+        view_shf = shift(img, (dy[i], dx[i]))
+        view_img = shift(img, (-dy[i], -dx[i]))
+        view_lbp = shift(lbp, (-dy[i], -dx[i]))
+        res = (view_img >= view_shf)
+        view_lbp |= (res.view(np.uint8) << i)
+
+    return lbp
+
+def shift(img, shift) -> np.ndarray:
+    r, c = shift[0], shift[1]
+
+    if r >= 0:
+        ret = img[r:, :]
+    else:
+        ret = img[0:r, :]
+
+    if c >= 0:
+        ret = ret[:, c:]
+    else:
+        ret = ret[:, 0:c]
+
+    return ret
